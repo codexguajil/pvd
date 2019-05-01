7a99b1adc3e929f4374340763f14347868baf1a7
Edgar Eloy Munoz <edgareloymunoz@gmail.com>
2019-05-01 11:40:19 -0600

    Main get from cities data

--- server.js
+++ server.js
@@ -10,3 +10,14 @@ app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
+//get city data from storage
+app.get('/api/v1/cities', (request, response) => {
+  database('cities').select()
+    .then((cities) => {
+      response.status(200).json(cities);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
+});
+
