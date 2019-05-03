a01c5efb126e44f24d7c729cc3055ee6c21f6c1b
Edgar Eloy Munoz <edgareloymunoz@gmail.com>
2019-05-03 09:28:44 -0600

    Knex

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
+const knex = require('knex')('production')
 
 const app = express()
 const port = 3000
